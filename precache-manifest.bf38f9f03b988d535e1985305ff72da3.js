79542206829da01f2720f928a5990821714c32ae
Rajibul Hasan <rajibulnsu@yahoo.com>
2020-05-09 20:35:12 +0200

    Updates

---
+++ precache-manifest.bf38f9f03b988d535e1985305ff72da3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5399954e1b044ecea5641df246d443ec",
+    "url": "/sumup-client-server/index.html"
+  },
+  {
+    "revision": "b602b08c21c39573a919",
+    "url": "/sumup-client-server/static/css/main.5d6cc05b.chunk.css"
+  },
+  {
+    "revision": "22aab86cf0ba53829351",
+    "url": "/sumup-client-server/static/js/2.86233c3e.chunk.js"
+  },
+  {
+    "revision": "b602b08c21c39573a919",
+    "url": "/sumup-client-server/static/js/main.afe6dffa.chunk.js"
+  },
+  {
+    "revision": "3ffb817a19d9e81da6c4",
+    "url": "/sumup-client-server/static/js/runtime~main.579b4f6f.js"
+  }
+]);
